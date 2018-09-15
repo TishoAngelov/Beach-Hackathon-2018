999bdadb76b83c54bc6b29e1af0d5ae4e32a5393
SamyShukerov <samy_shukerov@abv.bg>
2018-09-15 12:02:12 +0300

    added map component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetailMapComponent } from './detail-map/detail-map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailMapComponent
   ],
   imports: [
     BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBlGiVwuebeamPH77r6usLJxEfQahuL7Wo'
+    }),
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/detail-map/detail-map.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wis-detail-map',
+  templateUrl: './detail-map.component.html',
+  styleUrls: ['./detail-map.component.css']
+})
+export class DetailMapComponent implements OnInit {
+  lat: number = 51.678418;
+  lng: number = 7.809007;
+
+  ngOnInit() {
+  }
+
+  
+}
