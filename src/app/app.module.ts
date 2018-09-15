4aa3eb6ad8971d40bb464d262fd973a8e4a9915e
Mihail Dimitrov <Mihail_Dimitrov@epam.com>
2018-09-15 10:54:02 +0300

    initial components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
