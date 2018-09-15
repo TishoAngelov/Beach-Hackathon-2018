e23f9fb0d5d49cd599bf17ab4be001b90c6824b8
Tihomir Arahangelov <tihomir_arahangelov@epam.com>
2018-09-15 17:38:04 +0300

    fix cors issues

--- WhereIsTheSee/WhereIsTheSee.Api/App_Start/WebApiConfig.cs
+++ WhereIsTheSee/WhereIsTheSee.Api/App_Start/WebApiConfig.cs
@@ -13,9 +13,6 @@ namespace WhereIsTheSee.Api
   {
     public static void Register(HttpConfiguration config)
     {
-      var cors = new EnableCorsAttribute("*", "*", "*");
-      config.EnableCors(cors);
-
       // Web API configuration and services
       // Configure Web API to use only bearer token authentication.
       config.SuppressDefaultHostAuthentication();
